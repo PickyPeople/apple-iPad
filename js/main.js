98377f4c5d1d426c0f68a3986f749c8db9853485
pickyPeople <jjyjjh33@gmail.com>
2025-01-19 18:30:58 +0900

    project start

---
+++ js/main.js
@@ -0,0 +1,77 @@
+
+//장바구니
+const basketStarterEl = document.querySelector('header .basket-starter');
+const basketEl = basketStarterEl.querySelector('.basket');
+
+basketStarterEl.addEventListener("click", function (e) {
+  e.stopPropagation();
+  if (basketEl.classList.contains('show')) {
+    //show라는 클래스가 있으면 show클래스를 지움
+    hideBasket();
+  } else {
+    //show라는 클래스가 없으면 show 클래스를 추가해줌
+    showBasket();
+  }
+});
+
+basketEl.addEventListener('click', function (e) {
+  e.stopPropagation();
+});
+
+window.addEventListener('click', function () {
+  hideBasket();
+});
+
+
+function showBasket() {
+  basketEl.classList.add('show');
+}
+
+function hideBasket() {
+  basketEl.classList.remove('show');
+}
+
+// 검색!
+const headerEl = document.querySelector('header');
+const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')]
+const searchWrapEl = headerEl.querySelector('.search-wrap');
+const searchStarterEl = headerEl.querySelector('.search-starter');
+const searchCloserEl = searchWrapEl.querySelector('.search-closer');
+const searchShadowEl = searchWrapEl.querySelector('.shadow');
+const searchInputEl = searchWrapEl.querySelector('input');
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')];
+
+searchStarterEl.addEventListener('click', showSearch);
+
+searchCloserEl.addEventListener('click', hideSearch);
+
+searchShadowEl.addEventListener('click', hideSearch);
+
+function showSearch() {
+  headerEl.classList.add('searching');
+  document.documentElement.classList.add('fixed');
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
+  })
+  searchDelayEls.forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
+  })
+  setTimeout(function () {
+    searchInputEl.focus();
+  }, 600);
+};
+
+function hideSearch() {
+  headerEl.classList.remove('searching');
+  document.documentElement.classList.remove('fixed');
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
+  });
+
+  searchDelayEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
+  });
+
+  searchInputEl.value = '';
+  
+}
