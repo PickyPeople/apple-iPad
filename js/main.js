a9e082295992a3af537740672e50d76db0e537ac
장재영 <jangjaeyeong@jangjaeyeong-ui-MacBookPro.local>
2025-01-21 23:49:40 +0900

    add figcaption icon

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-
 //장바구니
-const basketStarterEl = document.querySelector('header .basket-starter');
-const basketEl = basketStarterEl.querySelector('.basket');
+const basketStarterEl = document.querySelector("header .basket-starter");
+const basketEl = basketStarterEl.querySelector(".basket");
 
 basketStarterEl.addEventListener("click", function (e) {
   e.stopPropagation();
-  if (basketEl.classList.contains('show')) {
+  if (basketEl.classList.contains("show")) {
     //show라는 클래스가 있으면 show클래스를 지움
     hideBasket();
   } else {
@@ -14,64 +13,62 @@ basketStarterEl.addEventListener("click", function (e) {
   }
 });
 
-basketEl.addEventListener('click', function (e) {
+basketEl.addEventListener("click", function (e) {
   e.stopPropagation();
 });
 
-window.addEventListener('click', function () {
+window.addEventListener("click", function () {
   hideBasket();
 });
 
-
 function showBasket() {
-  basketEl.classList.add('show');
+  basketEl.classList.add("show");
 }
 
 function hideBasket() {
-  basketEl.classList.remove('show');
+  basketEl.classList.remove("show");
 }
 
 // 검색!
-const headerEl = document.querySelector('header');
-const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')]
-const searchWrapEl = headerEl.querySelector('.search-wrap');
-const searchStarterEl = headerEl.querySelector('.search-starter');
-const searchCloserEl = searchWrapEl.querySelector('.search-closer');
-const searchShadowEl = searchWrapEl.querySelector('.shadow');
-const searchInputEl = searchWrapEl.querySelector('input');
-const searchDelayEls = [...searchWrapEl.querySelectorAll('li')];
+const headerEl = document.querySelector("header");
+const headerMenuEls = [...headerEl.querySelectorAll("ul.menu > li")];
+const searchWrapEl = headerEl.querySelector(".search-wrap");
+const searchStarterEl = headerEl.querySelector(".search-starter");
+const searchCloserEl = searchWrapEl.querySelector(".search-closer");
+const searchShadowEl = searchWrapEl.querySelector(".shadow");
+const searchInputEl = searchWrapEl.querySelector("input");
+const searchDelayEls = [...searchWrapEl.querySelectorAll("li")];
 
-searchStarterEl.addEventListener('click', showSearch);
+searchStarterEl.addEventListener("click", showSearch);
 
-searchCloserEl.addEventListener('click', hideSearch);
+searchCloserEl.addEventListener("click", hideSearch);
 
-searchShadowEl.addEventListener('click', hideSearch);
+searchShadowEl.addEventListener("click", hideSearch);
 
 function showSearch() {
-  headerEl.classList.add('searching');
-  document.documentElement.classList.add('fixed');
+  headerEl.classList.add("searching");
+  document.documentElement.classList.add("fixed");
   headerMenuEls.reverse().forEach(function (el, index) {
-    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
-  })
+    el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + "s";
+  });
   searchDelayEls.forEach(function (el, index) {
-    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
-  })
+    el.style.transitionDelay = (index * 0.4) / searchDelayEls.length + "s";
+  });
   setTimeout(function () {
     searchInputEl.focus();
   }, 600);
-};
+}
 
 function hideSearch() {
-  headerEl.classList.remove('searching');
-  document.documentElement.classList.remove('fixed');
+  headerEl.classList.remove("searching");
+  document.documentElement.classList.remove("fixed");
   headerMenuEls.reverse().forEach(function (el, index) {
-    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
+    el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + "s";
   });
 
   searchDelayEls.reverse().forEach(function (el, index) {
-    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
+    el.style.transitionDelay = (index * 0.4) / searchDelayEls.length + "s";
   });
 
-  searchInputEl.value = '';
-  
+  searchInputEl.value = "";
 }
