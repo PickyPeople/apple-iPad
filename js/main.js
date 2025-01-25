18932837eaecb239b5659cfea5da47dfe2b3208b
장재영 <jangjaeyeong@jangjaeyeong-ui-MacBookPro.local>
2025-01-25 22:10:29 +0900

    feat: select iPad JS Code Create

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import ipads from "../data/ipads.js";
+
 //장바구니
 const basketStarterEl = document.querySelector("header .basket-starter");
 const basketEl = basketStarterEl.querySelector(".basket");
@@ -73,37 +75,63 @@ function hideSearch() {
   searchInputEl.value = "";
 }
 
-
 //요소가 가시성 관찰(화면에 그 요소가 보이는지 안보이는지)
 const io = new IntersectionObserver(function (entries) {
   entries.forEach(function (entry) {
     if (!entry.isIntersecting) {
-      entry.target.classList.remove('show');
-      return
+      entry.target.classList.remove("show");
+      return;
     }
-    entry.target.classList.add('show');
-  })
-})
+    entry.target.classList.add("show");
+  });
+});
 
-const infoEls = document.querySelectorAll('.info');
+const infoEls = document.querySelectorAll(".info");
 infoEls.forEach(function (el) {
   io.observe(el);
-})
-
+});
 
 //비디오 재생!
-const video = document.querySelector('.stage video');
-const playBtn = document.querySelector('.stage .controller--play');
-const pauseBtn = document.querySelector('.stage .controller--pause');
+const video = document.querySelector(".stage video");
+const playBtn = document.querySelector(".stage .controller--play");
+const pauseBtn = document.querySelector(".stage .controller--pause");
 
-playBtn.addEventListener('click', function () {
+playBtn.addEventListener("click", function () {
   video.play();
-  playBtn.classList.add('hide');
-  pauseBtn.classList.remove('hide');
-})
+  playBtn.classList.add("hide");
+  pauseBtn.classList.remove("hide");
+});
 
-pauseBtn.addEventListener('click', function () {
+pauseBtn.addEventListener("click", function () {
   video.pause();
-  playBtn.classList.remove('hide');
-  pauseBtn.classList.add('hide');
-})
+  playBtn.classList.remove("hide");
+  pauseBtn.classList.add("hide");
+});
+
+//당신에게 맞는 iPad는? 렌더링!
+const itemsEl = document.querySelector("section.compare .items");
+ipads.forEach(function (ipad) {
+  const itemEl = document.createElement("div");
+  itemEl.classList.add("item");
+
+  let colorList = "";
+  ipad.colors.forEach(function (color) {
+    colorList += `<li style="background-color: ${color};"></li>`;
+  });
+
+  itemEl.innerHTML = /*html*/ `
+    <div class="thumbnail">
+      <img src="${ipad.thumbnail}" alt="${ipad.name}" />
+    </div>
+    <ul class="colors">
+      ${colorList}
+    </ul>
+    <h3 class="name">${ipad.name}</h3>
+    <p class="tagline">${ipad.tagline}</p>
+    <p class="price">₩${ipad.price.toLocaleString("en-US")}부터</p>
+    <button class="btn">구입하기</button>
+    <a href="${ipad.url}" class="link">더 알아보기</a>
+  `;
+
+  itemsEl.append(itemEl);
+});
